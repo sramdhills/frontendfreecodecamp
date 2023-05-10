55389692748f2c55804f78cb3f5eca97310a2475
pRogDeess <dhillsbest2000@gmail.com>
2023-05-11 03:05:00 +0800

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,15 @@
+const variable1 = 10;
+let variable2 = 10;
+var variable3 = 3;
+ //dont use var
+
+ const counter = 0;
+ counter++;
+ //const counter = 2;
+  /*we encounter problems of cannot redeclare
+  because const keyword makes variable unchanging and cannot be reassigned
+  the value
+
+  */
+
+const TAX_RATE = 0.08;
