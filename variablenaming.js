151afc38df728b655793c5f04e16d32c732e2448
pRogDeess <dhillsbest2000@gmail.com>
2023-05-12 00:37:41 +0800

    Create variablenaming.js

---
+++ variablenaming.js
@@ -0,0 +1,12 @@
+const yourfirstVariable = 10;
+const your_first_cariable = 10;
+const YOUR_FIRST_VARIABLE = 10;
+const yourfirstvariable = 10;
+const YourfirstVariable = 10;
+
+//camel case - YourfirstVariable
+//snake case - with underscores
+
+/*no number starting
+no spaces
+*/
