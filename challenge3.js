1e7f76851d888e82dd7251bbf9e3441f70dedb94
pRogDeess <dhillsbest2000@gmail.com>
2023-05-13 05:16:10 +0800

    Create challenge3.js

---
+++ challenge3.js
@@ -0,0 +1,8 @@
+const variable1 = 20;
+const variable2 = '40';
+const resultVariable = variable1 + variable2;
+
+console.log(resultVariable);
+typeof resultVariable
+
+//resultVariable is a number data type
