a5ba8999c877e6c414642b0d3d4698349277bf69
pRogDeess <dhillsbest2000@gmail.com>
2023-05-13 04:50:48 +0800

    Create challenge1.js

---
+++ challenge1.js
@@ -0,0 +1,15 @@
+const myBoolean = true; 
+const myString = 'hello world';
+const firstNumber = 20;
+const secondNumber = 40;
+let secondNumber1 = 40; 
+secondNumber1 = 80;
+
+const myArray = [(myBoolean), (myString)]; 
+
+const myObject = { 
+    firstProperty: myArray, sumProperty: firstNumber + secondNumber1
+}
+console.log(myObject);
+console.log(myObject.sumProperty); 
+console.log(myObject.firstProperty[1]); 
