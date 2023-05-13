099123df61168e4b6cdfb9f7ecd2fe313861fbb0
pRogDeess <dhillsbest2000@gmail.com>
2023-05-13 23:59:53 +0800

    Create operators.js

---
+++ Operators Challenges/operators.js
@@ -0,0 +1,11 @@
+//Arithmetic operators + - * /
+//Assignment operators = 
+//Comparison operators == === != !== > < >= <=
+
+//Ternary operators 
+const result = 20 == 20 ? 'values match' : 'values do not match';
+
+
+//Logical operators 
+&& || ! 
+20 === 20 && 10 == 10
