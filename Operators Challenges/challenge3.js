2c07cb5d8615cdc183bd5d6166fa962127cbaa44
pRogDeess <dhillsbest2000@gmail.com>
2023-05-13 23:40:38 +0800

    Create challenge3.js

---
+++ Operators Challenges/challenge3.js
@@ -0,0 +1,10 @@
+// const expression1 = 100 % 50;
+// const expression2 = 100 / 2;
+// const expression3 = expression1 < expression2;
+// const expression4 = expression3 && 300 + 5 === 305;
+// const expression5 = !expression4;
+
+// console.log(expression5);
+
+const expression5 = !((100 % 50) < (100 / 50) && (300 + 5 === 305))
+console.log(expression5);
