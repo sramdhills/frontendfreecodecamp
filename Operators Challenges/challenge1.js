056a6a6919d9b0f34e2bb14576d75263d7ab0cd4
pRogDeess <dhillsbest2000@gmail.com>
2023-05-13 23:40:27 +0800

    Create challenge1.js

---
+++ Operators Challenges/challenge1.js
@@ -0,0 +1,10 @@
+const numberVariable = 0;
+
+// numberVariable++
+// numberVariable++
+// numberVariable++
+//it didn't work because the variable is constant.Thus cannot be changed.
+
+console.log(numberVariable);
+
+//now it works!
