78a81a3f171f31e35b759dcb65627cd45c570faa
pRogDeess <dhillsbest2000@gmail.com>
2023-05-13 23:40:43 +0800

    Create challenge5.js

---
+++ Operators Challenges/challenge5.js
@@ -0,0 +1,16 @@
+const myObj = {
+    nestedObject1: {
+        price: 100,
+        quantity: 2
+    },
+    nestedObject2: {
+        price: 150,
+        quantity: 2
+    }
+};
+
+const myArray = [myObj.nestedObject1, myObj.nestedObject2];
+const result = 
+    (myArray[0].price * myArray[0].quantity) >
+    (myArray[1].price * myArray[1].quantity);
+console.log(result);
