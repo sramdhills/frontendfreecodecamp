49f876b5f5fcc21e66758e649881e7302c8f397e
pRogDeess <dhillsbest2000@gmail.com>
2023-05-21 12:17:09 +0800

    Update Loops.js

--- Functions,Iff,Loops/Loops.js
+++ Functions,Iff,Loops/Loops.js
@@ -1,3 +1,11 @@
-for (let i = 0; i < 100; i++) {
-    
+// for (let i = 0; i < 100; i++) {
+//     console.log(i);
+// }
+
+const arr = ['asdasd', 'asdasd', 'sdf', 'gkhdkj', 01, 20, 23, 432]
+
+for(let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === 'number'){
+        console.log(arr[i]);
+    }
 }
