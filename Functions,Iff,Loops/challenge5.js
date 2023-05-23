e04a2e4af1076dddea4f75ffcce4f1956f27f67b
pRogDeess <dhillsbest2000@gmail.com>
2023-05-23 15:08:39 +0800

    Create challenge5.js

---
+++ Functions,Iff,Loops/challenge5.js
@@ -0,0 +1,18 @@
+class SmallestIntegerFinder {
+    findSmallestInt(array) {
+      
+      let leastNumber;
+      for (let i = 0; i < array.length; i++) {
+        const arrNumber = array[i];
+      
+        if (i === 0){
+          leastNumber = arrNumber;
+        }
+        if(arrNumber < leastNumber){
+          leastNumber = arrNumber;
+        }
+      }
+        return leastNumber;
+    }
+  }
+  
