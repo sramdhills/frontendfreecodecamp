887557150ea60ba332764999c2ee9687a125a51f
pRogDeess <dhillsbest2000@gmail.com>
2023-05-24 13:53:03 +0800

    Create challenge13.js

---
+++ Functions,Iff,Loops/challenge13.js
@@ -0,0 +1,10 @@
+function maps(arr){
+    //() => {}
+    const result = arr.map((arrItem) => {
+      return arrItem * 2;
+    });
+    
+    return result;
+  }
+
+  //map method and arrow function within a function
