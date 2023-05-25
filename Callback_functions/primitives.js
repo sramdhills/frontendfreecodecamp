80df1916e89193afc733deb9404cd8252acc4eba
pRogDeess <dhillsbest2000@gmail.com>
2023-05-25 11:11:20 +0800

    Create primitives.js

---
+++ Callback_functions/primitives.js
@@ -0,0 +1,4 @@
+const string1 = newString('Hello, world!'); //object
+const string2 = 'Hellos, world!'; //primitives
+console.log(string1 === string2); //false
+console.log(string1 == string2); //true
