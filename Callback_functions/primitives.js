b1778e456044a0241dd4c0708f2bd51ef3acf1bb
pRogDeess <dhillsbest2000@gmail.com>
2023-05-25 14:06:13 +0800

    Update primitives.js

--- Callback_functions/primitives.js
+++ Callback_functions/primitives.js
@@ -1,4 +1,14 @@
-const string1 = newString('Hello, world!'); //object
+const string1 = new String('Hello, world!'); //object
 const string2 = 'Hellos, world!'; //primitives
 console.log(string1 === string2); //false
 console.log(string1 == string2); //true
+ 
+
+//6 DIFFERENT PRIMITIVES
+//PRMITIVE DATA TYPES WITH NO METHODS, NO FUNCTIONS
+
+'some string'.toUpperCase()
+
+//use string primitives!!
+
+//new keyword is an operator
