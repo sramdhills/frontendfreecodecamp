6633289ed85c398c2cf09321ae856e5720a17e8d
pRogDeess <dhillsbest2000@gmail.com>
2023-05-27 00:24:09 +0800

    Create slice&splice.js

---
+++ Array Methods/slice&splice.js
@@ -0,0 +1,9 @@
+const arr = [ 0, 1, 2, 4, 3, 6, 7]; 
+const copyArr = arr.slice()
+
+const anotherCopy = arr.slice(3) 
+
+//splice
+const Arrsplice = [1, 2, 3, 4, 6, 'oops']
+Arrsplice.splice(3, 2, 3.5);
+console.log(Arrsplice)
