8d6a1accf22f4569b597fa940605be675b0755e3
pRogDeess <dhillsbest2000@gmail.com>
2023-05-27 16:13:59 +0800

    Create Math.js

---
+++ Math Utilities/Math.js
@@ -0,0 +1,21 @@
+const myDate = new Date();
+myDate.getDate();
+
+Math.PI()
+Math.E()
+Math.abs(-23)
+Math.ceil(Math.PI) 
+Math.floor(Math.PI) 
+Math.round(Math.PI) 
+Math.min(3, 5, 1, 3) 
+Math.random() 
+
+const randomNumber = Math.random(); 
+const largerNumber = randomNumber * 100; 
+Math.floor(largerNumber); 
+
+const arr = ['some', 'values', 'blua', 'blah']; 
+const randomIndex = Math.floor(Math.random() * arr.length)
+console.log(arr[randomIndex]);
+
+jbsjdf
