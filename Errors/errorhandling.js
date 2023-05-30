fc46581c6257d7dc9a1fbad75221e27919d8cc08
pRogDeess <dhillsbest2000@gmail.com>
2023-05-30 22:11:14 +0800

    Update errorhandling.js

--- Errors/errorhandling.js
+++ Errors/errorhandling.js
@@ -1,6 +1,34 @@
 try {
-    var num = 2;
+    var nNum = 2;
     num.toUpperCase();
 } catch (error) {
-    console.log(errpr instanceof TypeError)
+    console.log(error instanceof TypeError)
+    console.log(error.message);
+}
+
+//Nan, null, undefined
+
+const myString = 'some string';
+Number('30')
+Number(myString) //Nan
+
+//null
+
+let num = null;
+
+
+if (num) {
+    console.log('this will not be reached');
+} else {
+    console.log('this will be reached');
+}
+
+//undefined
+let myvar;
+console.log(myvar)
+
+if (myvar) {
+    console.log('this will not be reached');
+} else {
+    console.log('falsy myvar');
 }
