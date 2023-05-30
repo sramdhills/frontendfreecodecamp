40e9c7d7ba32b7514e653c6c45f3fbffe57233e1
pRogDeess <dhillsbest2000@gmail.com>
2023-05-30 12:54:36 +0800

    Create errors.js

---
+++ Errors/errors.js
@@ -0,0 +1,20 @@
+//Reference Errors
+// not defined variable being printed
+const someVariable = 20;
+console.log(anotherVariable)
+
+//Syntax Errors
+const myObj = {
+    prop1: 'sone value';
+    prop2: 'another value'; //semicolons erase it
+}
+
+//Type Errors
+//incompatible type and methods
+
+const obj1 = { prop1: 20};
+const obj2 = { prop1: 40};
+return obj1 + obj2
+
+
+
