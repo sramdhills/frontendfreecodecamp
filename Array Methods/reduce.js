94782442d198f0522626cef8f12f794e6907ceeb
pRogDeess <dhillsbest2000@gmail.com>
2023-06-02 17:22:10 +0800

    Update reduce.js

--- Array Methods/reduce.js
+++ Array Methods/reduce.js
@@ -7,5 +7,15 @@ function reducerCallback(sum, arrItem){
 const result = arr.reduce(reducerCallback, 0); 
 console.log(result)
 
-const avearr = arr.reduce((sum, arrItem) => arrItem, 0) / arr.length 
-console.log(avearr)
+const avearr = arr.reduce((sum, arrItem) => sum + arrItem, 0) / arr.length;
+console.log(avearr);
+
+
+const array = [1, 3, 5, 6, 7];
+function reduceCallback(product, arrayItem){
+  return product += arrayItem;
+
+}
+
+const averResult = array.reduce(reduceCallback,0);
+console.log(averResult);
